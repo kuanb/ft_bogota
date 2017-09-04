693332ca159bbdd9f97a71217fc25d09cfebbb0c
Kuan Butts <kuanbutts@gmail.com>
2017-09-03 17:32:34 -0700

    latest methods, copy of pyeuclid and polyskel that are py3 friendly

--- ft_utils.py
+++ ft_utils.py
@@ -1,9 +1,17 @@
-from typing import Tuple, Union
+from typing import List, Tuple, Union
 
+import abc
 import geopandas as gpd
+import networkx as nx
+import numpy as np
 import pandas as pd
-from shapely.geometry import MultiPolygon, Point
+from shapely.geometry import MultiPolygon, Polygon, Point
+from shapely.geometry.polygon import orient
 from shapely.ops import triangulate
+
+import ft_polyskel
+import importlib
+importlib.reload(ft_polyskel)
 
 
 # TODO: Working in 4326 project is dumb. Should be using meter projection.
@@ -105,7 +113,7 @@ def get_similar_routes(reference_trip: MultiPolygon,
     return grouped_trips
 
 
-def unify_trips(grouped_trips: pd.Series):
+def unify_trips(grouped_trips: pd.Series) -> MultiPolygon:
     unioned = gpd.GeoSeries(grouped_trips).unary_union
 
     # If it is just a Poly, make it a MultiPoly
@@ -119,7 +127,7 @@ def unify_trips(grouped_trips: pd.Series):
     return MultiPolygon(polys).simplify(0.0001)
 
 
-def get_next_target_trip(df, use_vals):
+def get_next_target_trip(df: pd.DataFrame, use_vals: List[str]) -> str:
     curr_best = None
     curr_best_len = 0
     for tid in use_vals:
@@ -131,7 +139,11 @@ def get_next_target_trip(df, use_vals):
     return curr_best
 
 
-def triangulate_path_shape(path_shape: Union[Polygon, MultiPolygon]) -> gpd.GeoSeries:
+def triangulate_path_shape(path_shape: Union[Polygon, MultiPolygon]) -> List[Polygon]:
+    # Convert a Polygon into a MultiPoly if it is submitted as the path_shape
+    if not isinstance(path_shape, MultiPolygon):
+        path_shape = MultiPolygon([path_shape])
+
     # Flatten triangulations of each polygon in shape into a
     # one-level array of triangles
     t_list = []
@@ -145,8 +157,246 @@ def triangulate_path_shape(path_shape: Union[Polygon, MultiPolygon]) -> gpd.GeoS
     # Toss 
     tri_cleaned = []
     for poly in triangles:
-        if poly.centroid.intersects(MultiPolygon(polys)):
+        if poly.centroid.intersects(path_shape):
             tri_cleaned.append(poly)
 
-    return gpd.GeoSeries(tri_cleaned)
+    return tri_cleaned
+
+class NamedPoints(abc.ABC):
+    def __init__(self):
+        self.all_pts = [] # list of dicts
+
+    def get_nearest_node_id(self, curr_pt):
+        threshold = 0.00000001
+        this_pt_id = None
+
+        for pt in self.all_pts:
+            # If this is a point that already exists, then
+            # do not add a new point
+            if pt['pt'].distance(curr_pt) < threshold:
+                this_pt_id = pt['id']
+        return this_pt_id
+
+    def get_node_by_id(self, want_id):
+        for p in self.all_pts:
+            if p['id'] == want_id:
+                return p['pt']
+
+        return None
+    
+    def add(self, new_node):
+        self.all_pts.append(new_node)
+        return None
+    
+    def get_len(self):
+        return len(self.all_pts)
+
+def run_skeletonization(shape: Union[Polygon, MultiPolygon]):
+    # Convert a Polygon into a MultiPoly if it is submitted as the shape
+    if not isinstance(shape, MultiPolygon):
+        shape = MultiPolygon([shape])
+
+    # Prep the shape by pulling out the largest area from all
+    # of the shapes that are in the MPoly
+    curr_area = 0
+    curr_poly = None
+    for p in shape:
+        if p.area > curr_area:
+            curr_area = p.area
+            curr_poly = p
+    simple = curr_poly
+
+    # Convert the simple shape to only its exterior, so that we don't
+    # preserve any interior 'islands'
+    simple = Polygon(simple.exterior).buffer(0.005).simplify(0.005)
+
+    # Ensure that the shape's exterior ring is counter-clockwise
+    shape_oriented = orient(simple, sign=1.0)
+
+    sh_bounds = list(simple.exterior.coords)
+
+    try:
+        new_skeleton = ft_polyskel.skeletonize(sh_bounds, [])
+    except TypeError as e:
+        print('Caught error: {}'.format(e))
+        print('Attempting a try on a more simplified shape.')
+        new_skeleton = run_skeletonization(simple.buffer(0.005))
+
+    return new_skeleton
+
+
+def get_farthest_point_pair(skeleton) -> Tuple[Point]:
+    sg = nx.Graph()
+        
+    # First we need to create all unique points and update edges
+    init_point_id = 0
+
+    named_points = NamedPoints()
+    node_pos = {}
+    for edge in skeleton:
+        source = edge.source
+        
+        # Convert Point2 Pyeuclid types to standard Point
+        source_converted = Point(source[0], source[1])
+        
+        pts_to_eval = [source_converted]
+        for sink in edge.sinks:
+            sink_converted = Point(sink[0], sink[1])
+            pts_to_eval.append(sink_converted)
+        
+        for eval_pt in pts_to_eval:
+            this_pt_id = named_points.get_nearest_node_id(eval_pt)
+
+            if this_pt_id is None:
+                init_point_id += 1
+                this_pt_id = init_point_id
+
+                # Update the list
+                named_points.add({'id': this_pt_id, 'pt': eval_pt})
+
+                # And update the graph
+                sg.add_node(this_pt_id)
+                node_pos[this_pt_id] = (eval_pt.x, eval_pt.y)
+
+
+    # Now go ahead and add the edges by repeating the same loop, 
+    # with the named points all populated already
+    for edge in skeleton:
+        # Convert Point2 Pyeuclid types to standard Point
+        source = edge.source
+        source_converted = Point(source[0], source[1])
+        
+        # Start value
+        start_pt_id = named_points.get_nearest_node_id(source_converted)
+        
+        # Iterate through the end values
+        for sink in edge.sinks:
+            sink_converted = Point(sink[0], sink[1])
+            end_pt_id = named_points.get_nearest_node_id(sink_converted)
+            
+            edge_dist = source_converted.distance(sink_converted)
+            sg.add_edge(start_pt_id, end_pt_id, weight=edge_dist)
+
+    p = nx.shortest_path(sg)
+
+    curr_max_wt = 0
+    curr_path = None
+    for p1 in p.keys():
+        for p2 in p[p1].keys():
+            curr_p = p[p1][p2]
+
+            tot_wt = 0
+            for e1, e2 in zip(curr_p[:-1], curr_p[1:]):
+                wt = sg.edge[e1][e2]['weight']
+                tot_wt += wt
+        
+        if curr_max_wt < tot_wt:
+            curr_max_wt = tot_wt
+            curr_path = curr_p
+
+    first_point = Point(node_pos[curr_path[0]])
+    last_point = Point(node_pos[curr_path[-1]])
+
+    return (first_point, last_point)
+
+
+def great_circle_vec(lat1, lng1, lat2, lng2, earth_radius=6371009):
+    """
+    Vectorized function to calculate the great-circle distance between two points or between vectors of points.
+    Parameters
+    ----------
+    lat1 : float or array of float
+    lng1 : float or array of float
+    lat2 : float or array of float
+    lng2 : float or array of float
+    earth_radius : numeric
+        radius of earth in units in which distance will be returned (default is meters)
+    Returns
+    -------
+    distance : float or array of float
+        distance or vector of distances from (lat1, lng1) to (lat2, lng2) in units of earth_radius
+    """
+
+    phi1 = np.deg2rad(90 - lat1)
+    phi2 = np.deg2rad(90 - lat2)
+
+    theta1 = np.deg2rad(lng1)
+    theta2 = np.deg2rad(lng2)
+
+    cos = (np.sin(phi1) * np.sin(phi2) * np.cos(theta1 - theta2) + np.cos(phi1) * np.cos(phi2))
+
+    arc = np.arccos(cos)
+
+    # return distance in units of earth_radius
+    distance = arc * earth_radius
+    return distance
+
+
+def generate_multidigraph(tri_cleaned: List,
+                          start_pt: Point,
+                          end_pt: Point) -> nx.MultiDiGraph:
+    G = nx.MultiDiGraph()
+
+    node_count = 0
+
+    id_of_end_pt = None
+    id_of_start_pt = None
+
+    for tri in tri_cleaned:
+        coords = list(tri.exterior.coords)
+
+        ids = list(map(lambda x: node_count + x, [1, 2, 3]))
+        
+        for one_id, coord in zip(ids, coords[0:3]):
+            
+            thresh = 0.0001
+            if end_pt.distance(Point(*coord)) < thresh:
+                id_of_end_pt = one_id
+            if start_pt.distance(Point(*coord)) < thresh:
+                id_of_start_pt = one_id
+            
+            G.add_node(one_id, x=coord[0], y=coord[1])
+
+        for a, b, i in zip(coords[:-1], coords[1:], ids):
+            d = great_circle_vec(a[1], a[0], b[1], b[0])
+
+            a_id = i
+            b_id = i + 1
+
+            if b_id > ids[-1]:
+                b_id = ids[0]
+
+            G.add_edge(a_id, b_id, length = d)
+            G.add_edge(b_id, a_id, length = d)
+
+        node_count += 4
+
+    # Now we need to add connectors between each of the triangles
+    # from within the triangle collection
+    nodes_as_pts = []
+    node_ids = []
+
+    for node_id, xy in G.nodes(data=True):
+        nodes_as_pts.append(Point(xy['x'], xy['y']))
+        node_ids.append(node_id)
+
+    node_gdf = gpd.GeoDataFrame(node_ids, geometry=nodes_as_pts, columns=['node_id'])
+        
+    for node_id, xy in G.nodes(data=True):
+        node_pt = Point(xy['x'], xy['y'])
+        ds = node_gdf.distance(node_pt)
+        sub = node_gdf[ds < 0.0001]
+        
+        for to_id in sub['node_id'].values:
+            G.add_edge(node_id, to_id, length = 0.00001)
+            G.add_edge(to_id, node_id, length = 0.00001)
+    
+    # Update with some information for OSMnx if we need it
+    G.graph['crs'] = {'init':'epsg:4326'}
+    G.graph['name'] = 'gtfs_network'
+
+    return (G, id_of_end_pt, id_of_start_pt)
+
+
+
 
